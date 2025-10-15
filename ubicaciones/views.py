08e6d5b7f3b77b0a597f70c8a0239ed2a3ddf3b4
unknown <jab0n@outlook.com>
2025-10-15 11:39:16 -0600

    V0.4

--- ubicaciones/views.py
+++ ubicaciones/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Estado, Municipio
 
-# Create your views here.
+def index(request):
+    estados = Estado.objects.all()
+    return render(request, 'index.html', {'estados': estados})
+
+def get_municipios(request):
+    estado_id = request.GET.get('estado_id')
+    municipios = Municipio.objects.filter(estado_id=estado_id).values('id', 'nombre')
+    return JsonResponse(list(municipios), safe=False)
