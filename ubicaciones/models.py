231304a4ba7478fbc8a5c6af51beee670419564d
unknown <jab0n@outlook.com>
2025-10-15 11:36:20 -0600

    V0.2

--- ubicaciones/models.py
+++ ubicaciones/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Estado(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Municipio(models.Model):
+    nombre = models.CharField(max_length=100)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE, related_name='municipios')
+
+    def __str__(self):
+        return self.nombre
